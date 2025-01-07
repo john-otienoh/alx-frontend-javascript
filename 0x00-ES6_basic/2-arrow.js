21e349d19c4c32127232f531a673b579b212d094
john-otienoh <johnteclaire@gmail.com>
2025-01-07 10:06:15 +0300

    Es6 Basics Introduction

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,11 @@
+const add = (a, b) => a + b;
+console.log(add(5, 3));
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const self = this;
+  this.addNeighborhood = (newNeighborhood) => {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
+}
